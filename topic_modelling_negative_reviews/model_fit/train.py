2bc7824917ff9d4dbe6450c48d810b26d5f5ea2d
dbcordeiro <dbcordeiro@sefaz.al.gov.br>
2024-07-26 17:53:10 -0300

    Change relative path of variable_hyperparam.json

--- topic_modelling_negative_reviews/model_fit/train.py
+++ topic_modelling_negative_reviews/model_fit/train.py
@@ -12,12 +12,12 @@ from textblob import TextBlob
 import re
 import json
 
-with open('variable_hyperparam.json', 'r') as file:
+with open('../variable_hyperparam.json', 'r') as file:
     variable_hyperparam = json.load(file)
 
 SESSION: str = boto3.Session(
     profile_name=variable_hyperparam['profile_name'], 
-    region_name=variable_hyperparam['us-east-1']
+    region_name=variable_hyperparam['aws_region']
 )
 
 RUNTIME_CLIENT: str = boto3.client('runtime.sagemaker')
@@ -32,39 +32,9 @@ s3_bucket: str = variable_hyperparam['s3_bucket']
 SAVE_DATA_S3_PATH: str = f"{s3_bucket}/data/test_model"
 
 
-df = pd.read_parquet('../../data/interim/df_after_lang_detect.parquet')
-
-
-
-df = df[df['language'] != 'ERROR']
-df['title_text_review'] = df['title_text_review'].apply(lambda x: re.sub(r'[^\x00-\x7F]+', '', x))
-df['text_length'] = df['title_text_review'].swifter.apply(lambda x: len(x))
-df = df[df['text_length'] > 20]
-df = df[df['language'] == 'en']
-df = df.sample(1000000, random_state=42)
-df['review_sentiment'] = df['title_text_review'].swifter.apply(lambda x: TextBlob(x).sentiment.polarity)
-df = df[df['review_sentiment'] < -.3]
-df.drop(columns=['text_length', 'language', 'review_sentiment', 'parent_asin'], inplace=True)
-
-
-sentence_model = SentenceTransformer("all-MiniLM-L6-v2")
-embeddings = sentence_model.encode(df['title_text_review'].to_list())
-
-df['embeddings'] = list(embeddings)
-
-
-s3.to_parquet(
-    df=df,
-    path=SAVE_DATA_S3_PATH,
-    dataset=True,
-    filename_prefix="test_model_reviews_",
-    mode="overwrite",
-    boto3_session=SESSION
-)
-
 
 train_input = TrainingInput(
-    "s3://topic-modelling-reviews/data/test_model/",
+    "s3://topic-modelling-reviews/data/preprocessed_reviews/",
     content_type="application/x-parquet"
 )
 
@@ -73,7 +43,7 @@ estimator = Estimator(
     image_uri=image_uri_model,
     role=role,
     instance_count=1,
-    instance_type="ml.c5.2xlarge",
+    instance_type="ml.c5.4xlarge",
     sagemaker_session=SAGEMAKER_SESSION,
     base_job_name="model"
 )
