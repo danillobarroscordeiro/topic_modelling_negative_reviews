c20f09a078e51e7d79af6e127a1e9e7589e7c4b4
dbcordeiro <dbcordeiro@sefaz.al.gov.br>
2024-08-01 21:17:39 -0300

    Include logger. Change representation docs format

--- topic_modelling_negative_reviews/api/api.py
+++ topic_modelling_negative_reviews/api/api.py
@@ -1,11 +1,13 @@
 import gradio as gr
 import requests
 import json
+import logging
+
+logger = logging.getLogger(__name__)
 
 with open('variable_hyperparam.json', 'r') as file:
     variable_hyperparam = json.load(file)
 
-#endpoint = "https://dk5nrp46w2af4blzrctvyuqvfe0lgmhz.lambda-url.us-east-1.on.aws/"
 
 def request_words(product_id: str) -> str:
 
@@ -15,10 +17,18 @@ def request_words(product_id: str) -> str:
     )
     
     if response.status_code == 200:
-        cluster_words = response.json()['output_data']['cluster_words']
         product_name = response.json()['output_data']['product_name']
+        topic_words = response.json()['output_data']['topic_words']
+        representative_docs = response.json()['output_data']['representative_docs']
+        formatted_rep_docs = []
+        for i, doc in enumerate(representative_docs, 1):  # Start counting from 1
+            formatted_rep_docs.append(f"{i}) {doc}")
+        formatted_rep_docs = "\n\n".join(formatted_rep_docs)
 
-        return cluster_words, product_name
+        formatted_product_name = f"**Product Name**: {product_name}"
+        formmated_topic_words = f"Top words describing negative reviews are: **{topic_words}**"
+        formmated_representative_docs = f"**Representative reviews**: \n\n {formatted_rep_docs}" 
+        return formatted_product_name, formmated_topic_words, formmated_representative_docs
 
     else:
         return f"Error: {response.status_code}", response.text
@@ -27,9 +37,14 @@ def create_api():
     with gr.Blocks() as api:
         product_id = gr.Textbox(label="Type product ID")
         submit_button = gr.Button("Submit")
-        product_name = gr.Markdown(label="Product Name:")
-        cluster_words = gr.Markdown(label="The top words that describes the negatives reviews are:")
-        submit_button.click(fn=request_words, inputs=product_id, outputs=[product_name, cluster_words])
+        product_name = gr.Markdown()
+        topic_words = gr.Markdown()
+        formmated_representative_docs = gr.Markdown()
+        submit_button.click(
+            fn=request_words, 
+            inputs=product_id, 
+            outputs=[product_name, topic_words, formmated_representative_docs]
+            )
 
     api.launch(server_port=7860, server_name='0.0.0.0')
 
